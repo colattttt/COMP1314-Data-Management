4aafcdb468504def8608ca26f4e72b32810fa65c
colattttt <pohshiulim@gmail.com>
2025-12-01 23:41:24 +0800

    first commit

---
+++ goldpricetracker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Progress Bar
+echo -n "Fetching live data "
+for i in {1..20}; do
+    echo -n "▮"
+    sleep 0.05
+done
+echo -e "  Done ✓"
+echo
+
+# Fetch HTML
+curl -sS https://www.kitco.com/charts/gold > raw.html
+
