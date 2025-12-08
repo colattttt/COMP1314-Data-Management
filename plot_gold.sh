7dfab94b452f7631146bfe6c77226932ac3cc951
colattttt <pohshiulim@gmail.com>
2025-12-08 18:06:51 +0800

    eighteen commit

--- plot_gold.sh
+++ plot_gold.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-plot_usd_price() {
+plot_ask() { 
+    currency=$1
+    lower=$(echo "$currency" | tr 'A-Z' 'a-z')
+    outfile="${lower}_ask_price.png"
 
-    mysql -u root -p1234 -N -B -e \
+    mysql -u root -p1234 -D gold_tracker -N -B -e \
     "SELECT DATE(timestamp_local), TIME(timestamp_local), ask
-     FROM gold_tracker.gold_prices 
-     WHERE currency='USD' 
-     ORDER BY timestamp_local" > plotdata.txt
+     FROM gold_prices 
+     WHERE currency_id = (SELECT currency_id FROM currencies WHERE currency_code='$currency')
+     ORDER BY timestamp_local" > plotdata_ask.txt
 
 gnuplot << EOF
 set terminal png size 1920,1080
-set output "usd_price.png"
-set title "USD Gold Ask Price Over Time"
+set output "$outfile"
+set title "$currency Gold Ask Price Over Time"
 
 set xdata time
 set timefmt "%Y-%m-%d %H:%M:%S"
 set format x "%m/%d\n%H:%M"
 
 set xlabel "Time"
-set ylabel "USD"
+set ylabel "Ask Price ($currency)"
 
-plot "plotdata.txt" using (strcol(1)." ".strcol(2)):3 \
+plot "plotdata_ask.txt" using (strcol(1)." ".strcol(2)):3 \
      with linespoints lt rgb 'purple' lw 2 title 'Ask Price'
 EOF
 
-echo "Generated usd_price.png"
+    echo "Generated $outfile"
 }
-
-case "$1" in
-  usd_price)
-    plot_usd_price
-    ;;
-  *)
-    echo "Usage: ./plot.sh usd_price"
-    ;;
-esac
