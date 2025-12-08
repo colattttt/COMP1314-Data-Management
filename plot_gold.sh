b78a689ce9c8ee2cdd7ee1c9307466095aad928c
colattttt <pohshiulim@gmail.com>
2025-12-08 21:15:50 +0800

    twenty commit

--- plot_gold.sh
+++ plot_gold.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Plot ask price function
 plot_ask() { 
     currency=$1
     lower=$(echo "$currency" | tr 'A-Z' 'a-z')
@@ -30,6 +31,7 @@ EOF
     echo "Generated $outfile"
 }
 
+# Plot bid price function
 plot_bid() {
     currency=$1
     lower=$(echo "$currency" | tr 'A-Z' 'a-z')
@@ -56,6 +58,38 @@ set ylabel "Bid Price ($currency)"
 
 plot "plotdata_bid.txt" using (strcol(1)." ".strcol(2)):3 \
      with linespoints lt rgb 'blue' lw 2 title 'Bid Price'
+EOF
+
+    echo "Generated $outfile"
+}
+
+# Plot high price function
+plot_high() { 
+    currency=$1
+    lower=$(echo "$currency" | tr 'A-Z' 'a-z')
+    outfile="${lower}_high_price.png"
+
+    # Export DATE, TIME, HIGH
+    mysql -u root -p1234 -D gold_tracker -N -B -e \
+    "SELECT DATE(timestamp_local), TIME(timestamp_local), high
+     FROM gold_prices 
+     WHERE currency_id = (SELECT currency_id FROM currencies WHERE currency_code='$currency')
+     ORDER BY timestamp_local" > plotdata_high.txt
+
+gnuplot << EOF
+set terminal png size 1920,1080
+set output "$outfile"
+set title "$currency Gold High Price Over Time"
+
+set xdata time
+set timefmt "%Y-%m-%d %H:%M:%S"
+set format x "%m/%d\n%H:%M"
+
+set xlabel "Time"
+set ylabel "High Price ($currency)"
+
+plot "plotdata_high.txt" using (strcol(1)." ".strcol(2)):3 \
+     with linespoints lt rgb 'orange' lw 2 title 'High Price'
 EOF
 
     echo "Generated $outfile"
